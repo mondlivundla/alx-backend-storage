4869be602900ec8143ae303ae8bfb95f472edf25
mondlivundla <mondlivundla7@gmail.com>
2023-07-19 03:53:27 +0200

    update 102-log_stats.py

--- 0x01-NoSQL/102-log_stats.py
+++ 0x01-NoSQL/102-log_stats.py
@@ -1,54 +1,42 @@
 #!/usr/bin/env python3
-"""
-Aggregation operations
-"""
+""" Log stats """
 from pymongo import MongoClient
-from collections import OrderedDict
-from typing import Tuple
 
 
-def get_nginx_stats() -> Tuple:
-    """
-    Queries nginx collection for specific data
-    - Returns:
-        - count of all documents
-        - count of each method in the collection
-        - count of each GET calls to /status path
-        - count of top 10 visited ips
-    """
-    client: MongoClient = MongoClient()
-    db = client.logs
-    collection = db.nginx
-    methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
-    method_stats = []
-    for method in methods:
-        method_count = collection.count_documents({'method': method})
-        method_stats.append({'method': method, 'count': method_count})
-    doc_count = collection.estimated_document_count()
-    status_path_stats = collection.count_documents({'method': 'GET',
-                                                    'path': '/status'})
-    pipeline = [{'$group': {'_id': '$ip', 'count': {'$sum': 1}}},
-                {'$sort': OrderedDict([('count', -1)])},
-                {'$limit': 10}]
-    top_ips = collection.aggregate(pipeline)
-    client.close()
-    return doc_count, method_stats, status_path_stats, top_ips
+if __name__ == "__main__":
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    db_nginx = client.logs.nginx
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
 
+    count_logs = db_nginx.count_documents({})
+    print(f'{count_logs} logs')
 
-def print_nginx_stats() -> None:
-    """
-    Prints stats from nginx query
-    """
-    doc_count, method_stats, status_path_stats, top_ips = get_nginx_stats()
-    print(f'{doc_count} logs')
     print('Methods:')
-    for method in method_stats:
-        print(f'\tmethod {method.get("method")}: {method.get("count")}')
-    print(f'{status_path_stats} status check')
-    print('IPs:')
-    for ip in top_ips:
-        print(f'\t{ip.get("_id")}: {ip.get("count")}')
+    for method in methods:
+        count_method = db_nginx.count_documents({'method': method})
+        print(f'\tmethod {method}: {count_method}')
 
+    check = db_nginx.count_documents(
+        {"method": "GET", "path": "/status"}
+    )
 
-if __name__ == '__main__':
-    print_nginx_stats()
+    print(f'{check} status check')
+    print("IPs:")
+    ips = db_nginx.aggregate([
+        {"$group":
+            {
+                "_id": "$ip",
+                "count": {"$sum": 1}
+            }
+        },
+        {"$sort": {"count": -1}},
+        {"$limit": 10},
+        {"$project": {
+            "_id": 0,
+            "ip": "$_id",
+            "count": 1
+        }}
+    ])
+
+    for ip in ips:
+        print(f"\t{ip.get('ip')}: {ip.get('count')}")
